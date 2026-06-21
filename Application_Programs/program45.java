55b36eb9855747bc8448e3670e93a6cefdfdcc72
shraddhhhaaa <shraddhamune8@gmail.com>
2026-06-22 00:01:17 +0530

    initial upload

---
+++ Application_Programs/program45.java
@@ -0,0 +1,44 @@
+//TYPE 3
+//to check whether the number eentered is divisible by 3 and 5
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean CheckDivisible(int iNo)
+    {
+         if((iNo % 3 == 0) && (iNo % 5 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program45
+{
+    
+    public static void main(String A[])
+    {
+       Scanner sobj = new Scanner(System.in);
+
+       int iValue = 0;
+       boolean bRet = false;
+
+       System.out.println("Enter Number : ");
+       iValue = sobj.nextInt();
+
+       NumberX nobj = new NumberX(); 
+       bRet = nobj.CheckDivisible(iValue);
+    
+       if(bRet == true)
+       {
+        System.out.println("Number is divisible by 3 and 5");
+       } 
+       else{
+        System.out.println("Number is not divisible by 3 and 5");
+       }
+    }
+}
