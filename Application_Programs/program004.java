5931e84171de34cc8679bb123fd2365aee0dd4fa
shraddhhhaaa <shraddhamune8@gmail.com>
2026-07-01 23:04:51 +0530

    initial upload

---
+++ Application_Programs/program004.java
@@ -0,0 +1,13 @@
+//to print 1-10 without taking input
+class program37
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt =1; iCnt <= 10; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+}
---
+++ Application_Programs/program005.java
@@ -0,0 +1,13 @@
+//to print 1-10 without taking input
+class program38
+{
+    public static void main(String A[])
+    {
+        int iCnt = 0;
+
+        for(iCnt = 1; iCnt <= 10; iCnt++)
+        {
+            System.out.printf("%d\n",iCnt);
+        }
+    }
+}
